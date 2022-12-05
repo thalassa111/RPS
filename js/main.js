ff4bbbc09b26ccf6456c8b7ac81bd40bc362d791
unknown <cheng_taung_wu@hotmail.com>
2022-12-05 21:08:54 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function testFunction(){
+    /* alert("testar funktionen"); */
+    /* document.getElementById("canvasDiv").remove(); */
+    /* document.querySelectorAll(".grid-container").forEach(el => el.remove()); */
+    document.querySelector("#grid-container").remove();
+/*     if(document.getElementById("canvasDiv"). hidden){
+        document.getElementById("canvasDiv"). hidden = false;
+    }
+    else{
+        document.getElementById("canvasDiv"). hidden = true;
+    } */
+}
